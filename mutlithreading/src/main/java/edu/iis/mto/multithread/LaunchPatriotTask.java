dbfd4e8269f826cc2f64a817095c86f10bbd6c74
CheretKa <ocheretianfws@gmail.com>
2021-04-22 03:49:16 +0300

    added class LaunchPatriotTask

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/LaunchPatriotTask.java
@@ -0,0 +1,22 @@
+package edu.iis.mto.multithread;
+
+
+class LaunchPatriotTask implements Runnable {
+
+    private Scud enemyMissle;
+    private int rocketCount;
+    private PatriotBattery battery;
+
+    public LaunchPatriotTask(Scud enemyMissle, int rocketCount, PatriotBattery battery) {
+        this.enemyMissle = enemyMissle;
+        this.rocketCount = rocketCount;
+        this.battery = battery;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < rocketCount; i++) {
+            battery.launchPatriot(enemyMissle);
+        }
+    }
+}
