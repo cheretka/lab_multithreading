de61fc1c83c39c02c85bf7ba8a298e710d3ca929
CheretKa <ocheretianfws@gmail.com>
2021-04-22 03:48:57 +0300

    added class BetterRadar

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,20 @@
+package edu.iis.mto.multithread;
+
+import java.util.concurrent.Executor;
+
+public class BetterRadar {
+
+    private int rocketCount;
+    private PatriotBattery battery;
+    private Executor executor;
+
+    public BetterRadar(int rocket_count, PatriotBattery battery, Executor executor) {
+        this.rocketCount = rocket_count;
+        this.battery = battery;
+        this.executor = executor;
+    }
+
+    public void notice(Scud enemyMissle) {
+        executor.execute(new LaunchPatriotTask(enemyMissle, rocketCount, battery));
+    }
+}
