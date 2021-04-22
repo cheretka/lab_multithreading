c3488c9f2b79cc11e51993141ccf6a74b79d1826
CheretKa <ocheretianfws@gmail.com>
2021-04-22 04:38:43 +0300

    added tests

--- mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -9,12 +9,18 @@ public class BetterRadar {
     private Executor executor;
 
     public BetterRadar(int rocket_count, PatriotBattery battery, Executor executor) {
+        if(rocket_count<1)
+            rocket_count = 1;
         this.rocketCount = rocket_count;
         this.battery = battery;
         this.executor = executor;
     }
 
     public void notice(Scud enemyMissle) {
+        launchPatriot(enemyMissle, rocketCount);
+    }
+
+    private void launchPatriot(Scud enemyMissle, int rocketCount) {
         executor.execute(new LaunchPatriotTask(enemyMissle, rocketCount, battery));
     }
 }
---
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -0,0 +1,50 @@
+
+package edu.iis.mto.multithread;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.concurrent.Executor;
+
+
+
+@ExtendWith(MockitoExtension.class)
+class BetterRadarTest {
+
+    @Mock
+    private PatriotBattery batteryMock;
+
+    private Executor executor;
+    private int rocketCount;
+    private Scud enemyMissle;
+
+    @BeforeEach
+    void setup() {
+        executor = Runnable::run;
+        enemyMissle = new Scud();
+    }
+
+    @RepeatedTest(100)
+    void when_TenRocketsInRadar_Expect_CallLaunchPatriotTenTimes() {
+        rocketCount = 10;
+        BetterRadar radar = new BetterRadar(rocketCount, batteryMock, executor);
+
+        radar.notice(enemyMissle);
+        verify(batteryMock, times(rocketCount)).launchPatriot(enemyMissle);
+    }
+
+    @RepeatedTest(100)
+    void when_LessThanOneRocketInRadar_Expect_CallLaunchPatriotOnes() {
+        rocketCount = 0;
+        BetterRadar radar = new BetterRadar(rocketCount, batteryMock, executor);
+
+        radar.notice(enemyMissle);
+        verify(batteryMock, atLeastOnce()).launchPatriot(enemyMissle);
+    }
+
+}
